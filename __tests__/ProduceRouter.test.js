996b642e5886f5e53fb6b0fc48e251ee2e0d6dc7
Miraan Tabrez <miraan100@yahoo.co.uk>
2017-12-27 18:55:52 +0000

    Add tests for getting produce by id, and add new route to get produce by id

--- __tests__/ProduceRouter.test.js
+++ __tests__/ProduceRouter.test.js
@@ -10,26 +10,61 @@ describe('Node Flow API', () => {
 
     it('should return a JSON array', () =>
       request(app).get('/api/v1/produce')
-        .expect(200)
-        .then(res => expect(res.body).toBeInstanceOf(Array)))
+      .expect(200)
+      .then(res => expect(res.body).toBeInstanceOf(Array)))
 
     it('should return objects with the correct props', () =>
       request(app).get('/api/v1/produce')
-        .expect(200)
-        .then(res => {
-          let sampleKeys = Object.keys(res.body[0])
-          expectedProps.forEach(key =>
-            expect(sampleKeys.includes(key)).toBe(true))
-        }))
+      .expect(200)
+      .then(res => {
+        let sampleKeys = Object.keys(res.body[0])
+        expectedProps.forEach(key =>
+          expect(sampleKeys.includes(key)).toBe(true))
+      }))
 
     it('shouldn\'t return objects with extra props', () =>
       request(app).get('/api/v1/produce')
-        .expect(200)
-        .then(res => {
-          let extraProps = Object.keys(res.body[0]).filter(key =>
-            !expectedProps.includes(key))
-          expect(extraProps.length).toBe(0)
-        }))
+      .expect(200)
+      .then(res => {
+        let extraProps = Object.keys(res.body[0]).filter(key =>
+          !expectedProps.includes(key))
+        expect(extraProps.length).toBe(0)
+      }))
+  })
+
+  describe('GET /api/v1/produce/:id - get produce item by id', () => {
+    it('should return an object of type Produce', () =>
+      request(app).get('/api/v1/produce/1')
+      .expect(200)
+      .then(res => {
+        const reqKeys = ['id', 'name', 'price', 'quantity']
+        const { item } = res.body;
+        reqKeys.forEach(key => expect(Object.keys(item)).toContain(key))
+        expect(typeof item.id).toBe('number')
+        expect(typeof item.name).toBe('string')
+        expect(typeof item.quantity).toBe('number')
+        expect(typeof item.price).toBe('number')
+      }))
+
+    it('should return a Produce with the requested id', () =>
+      request(app).get('/api/v1/produce/1')
+      .expect(200)
+      .then(res => expect(res.body.item).toEqual({
+        id: 1,
+        name: 'Carrots',
+        quantity: 3,
+        price: 0.79,
+      })))
+
+    it('should 400 on a request for a nonexistent id', () =>
+      Promise.all([
+        request(app).get('/api/v1/produce/-32')
+        .expect(400)
+        .then(res => expect(res.body.message).toBe('No item found with id: -32')),
+        request(app).get('/api/v1/produce/99999')
+        .expect(400)
+        .then(res => expect(res.body.message).toBe('No item found with id: 99999'))
+      ]))
   })
 
 })
--- src/routers/ProduceRouter.js
+++ src/routers/ProduceRouter.js
@@ -15,9 +15,26 @@ export default class ProduceRouter {
 
   init(): void {
     this.router.get('/', this.getAll)
+    this.router.get('/:id', this.getById)
   }
 
   getAll(req: $Request, res: $Response): void {
     res.status(200).json(inventory)
+  }
+
+  getById(req: $Request, res: $Response): void {
+    const id = parseInt(req.params.id, 10)
+    const record = inventory.find(item => item.id === id)
+    if (record) {
+      res.status(200).json({
+        message: 'Success',
+        item: record
+      })
+    } else {
+      res.status(400).json({
+        status: res.status,
+        message: `No item found with id: ${id}`
+      })
+    }
   }
 }
